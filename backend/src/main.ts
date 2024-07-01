55888ed86d6516522f33fefebe89ecaa438060bd
tristanB12 <tristan.bourgeois@epitech.eu>
2024-07-01 20:11:46 +0200

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
-  app.enableCors();
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(port);
   console.log(`Server running on port ${port}`);
