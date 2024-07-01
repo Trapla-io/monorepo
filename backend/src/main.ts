284c0ec3f0023df592ef59e8d4209d2a4ccd3f98
tristanB12 <tristan.bourgeois@epitech.eu>
2024-07-01 20:15:45 +0200

    feat: remove cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
-  app.enableCors({ origin: '*' });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(port);
   console.log(`Server running on port ${port}`);
