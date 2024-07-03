509df81d763ccefc6834196749e7fe0b184d3a97
tristanB12 <tristan.bourgeois@epitech.eu>
2024-07-03 13:57:23 +0200

    feat: add theme in travel book update dto

--- backend/src/travel-books/dto/update-travel-book.dto.ts
+++ backend/src/travel-books/dto/update-travel-book.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTravelBookDto } from './create-travel-book.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateTravelBookDto extends PartialType(CreateTravelBookDto) {}
+export class UpdateTravelBookDto extends PartialType(CreateTravelBookDto) {
+  @IsOptional()
+  theme: object;
+}
